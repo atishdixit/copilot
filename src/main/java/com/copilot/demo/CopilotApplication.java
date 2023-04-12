0ae769aea621736c19439cbba03c7746abc0aa4e
atishdixit <atishdixit26@gmail.com>
2023-04-12 10:24:21 +0530

    Initial commit

---
+++ src/main/java/com/copilot/demo/CopilotApplication.java
@@ -0,0 +1,13 @@
+package com.copilot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotApplication.class, args);
+	}
+
+}
